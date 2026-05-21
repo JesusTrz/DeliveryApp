25faa4b7db543755b77ab8be8faeb37a14396331
JesusTrz <yisusgonzalez2315@gmail.com>
2026-05-21 08:04:29 -0600

    APP-1 Pantalla de Login

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { CartProvider } from './context/CartContext'
+import Login from './views/Login/Login'
+import Home from './views/Home/Home'
+import Search from './views/Search/Search'
+import Orders from './views/Orders/Orders'
+import Profile from './views/Profile/Profile'
+import Cart from './views/Cart/Cart'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Login />,
+  },
+  {
+    path: '/home',
+    element: <Home />,
+  },
+  {
+    path: '/search',
+    element: <Search />,
+  },
+  {
+    path: '/orders',
+    element: <Orders />,
+  },
+  {
+    path: '/profile',
+    element: <Profile />,
+  },
+  {
+    path: '/cart',
+    element: <Cart />,
+  },
+])
+
+function App() {
+  return (
+    <CartProvider>
+      <RouterProvider router={router} />
+    </CartProvider>
+  )
+}
+
+export default App
